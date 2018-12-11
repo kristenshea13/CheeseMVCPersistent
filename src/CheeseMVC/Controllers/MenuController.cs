7f70a775ffa59e9ed2ca90043cce9c657234b68d
Kristen Shea <kstar61@live.com>
2018-12-10 19:18:09 -0600

    *

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+
+
+
+    }
+}
