17b9e8a987c72fe5a3680d4238992e26af9b87e8
Kristen Shea <kstar61@live.com>
2018-12-12 16:53:42 -0600

    *

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace CheeseMVC.Models
         public string Description { get; set; }
         public CheeseCategory Category { get; set; }
         public int CategoryID { get; set; }
+
+        
         public int ID { get; set; }
 
     }
